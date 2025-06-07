224a002ff4140c1eb193e553d92a1bd8e314269b
samblackspy <apple.rnb@gmail.com>
2025-06-07 14:30:38 +0530

    adding vercel.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
